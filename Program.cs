e0c1d036c07a3937a98cd4c0cf69eeb9e0149b1e
evgeniy.golykhovskiy <evgeniy.golykhovskiy@akvelon.com>
2022-10-27 14:26:45 +0300

    Add electron support

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
+using ElectronNET.API;
 using jira2ets.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseElectron(args);
+
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddElectron();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,5 +30,9 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+Task.Run(() => {
+    var window =  Electron.WindowManager.CreateWindowAsync();
+});
 
 app.Run();
